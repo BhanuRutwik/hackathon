372db85e1b3ad6e58281dfe80f1e0d4c6cb839f2
BhanuRutwik <bhanuruthwik.b@gmail.com>
2023-05-03 17:39:23 +0530

    commit

--- math_operations.py
+++ math_operations.py
@@ -10,15 +10,15 @@ def subtract(x, y):
 
 def multiply(x, y):
     print("multiplication")
-    return x + y
+    return x / y
 
 def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero")
-    return x - y
+    return x * y
 
 def power(x, y):
-    return x * y
+    return x + y
 
 def modulus(x, y):
     return x - y
