592b82c820eb9d48ef13fa224f62e6973362e47c
Bulusu <sbulusu@informatica.com>
2023-05-03 16:58:47 +0530

    commit

--- math_operations.py
+++ math_operations.py
@@ -1,22 +1,24 @@
 def add(x, y):
     print("Addition operation")
+    print('test1')
     return x - y
 
 def subtract(x, y):
     print("subtraction")
+    print('test2')
     return x * y
 
 def multiply(x, y):
     print("multiplication")
-    return x % y
+    return x + y
 
 def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero")
-    return x / y
+    return x - y
 
 def power(x, y):
-    return x ** y
+    return x * y
 
 def modulus(x, y):
-    return x % y
+    return x / y
--- string_operations.py
+++ string_operations.py
@@ -1,21 +1,24 @@
 def reverse_string(s):
-    print('reverse')
+    print('reverseadsf')
     return s[::-1]
 
 def to_uppercase(s):
-    print('uppercase')
+    print('uppercaseadsf')
     return s.upper()
 
 def to_lowercase(s):
+    print('adsf')
     return s.lower()
 
 def is_palindrome(s):
     s = s.lower()
     s = ''.join(filter(str.isalnum, s))
+    print('adsf')
     return s == s[::-1]
 
 def count_vowels(s):
     print('vowels')
+    print('adsf')
     vowels = 'aeiou'
     count = 0
     for char in s.lower():
